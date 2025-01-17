f82e38d167d6d54cd91b9f1370dccfaa5e6ad8d7
SujithVSuresh <sujitheternal13@gmail.com>
2025-01-17 13:27:26 +0530

    done the basic setup for the signup controller

---
+++ backend/controllers/userController.js
@@ -0,0 +1,40 @@
+import User from "../models/userModel";
+import asyncHandler from "express-async-handler";
+import generateTokens from "../utils/generateTokens";
+
+// @desc    Register a new user
+// route    POST /api/user/signup
+// @access  Public
+const signup = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  const existingUser =
+    (await User.findOne({ email })) || (await User.findOne({ username }));
+
+  if (existingUser) {
+    if (existingUser.verified) {
+      //sends an error message if verified user already exists.
+      res.status(409);
+      throw new Error("User already exists.");
+    } else {
+      //deleting that user if he is unverified.
+      existingUser.deleteOne();
+    }
+  }
+
+  const user = await User.create({ username, email, password });
+  if (user) {
+    generateTokens(res, user._id);
+    res.status(201).json({
+      success: true,
+      message: "Account has been created successfully.",
+    });
+    console.log(`✨ [register] @${user.username} has registered an account.`);
+    //todo
+    //generate verification code
+    //set verification code
+    //generate verification link
+    //send verification email
+  } else {
+  }
+});
